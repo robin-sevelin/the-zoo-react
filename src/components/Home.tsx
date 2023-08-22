88aa2001fb8325a4c7bb7e0e6b6e90691f1ad9cd
robin-sevelin <robinsevelin@hotmail.com>
2023-08-22 22:46:45 +0200

    added some css

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,17 +1,19 @@
 export const Home = () => {
   return (
     <div className='home'>
-      <h2>Välkommen till "The Zoo"</h2>
-      <p>
-        Se till att djuren är matade och passa på att läsa lite fakta om dom.
-      </p>
-      <img
-        height={300}
-        width={300}
-        src='/src/assets/assetstream.webp'
-        alt='dog-food'
-      />
-      <p>OBS! Denna sida har inte sponsrats av Cesar</p>
+      <div className='filter'>
+        <h2>Välkommen till "The Zoo"</h2>
+        <p>
+          Se till att djuren är matade och passa på att läsa lite fakta om dom.
+        </p>
+        <img
+          height={300}
+          width={300}
+          src='/src/assets/assetstream.webp'
+          alt='dog-food'
+        />
+        <p>OBS! Denna sida har inte sponsrats av Cesar</p>
+      </div>
     </div>
   );
 };
